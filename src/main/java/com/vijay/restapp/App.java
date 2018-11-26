5062f61fb09a2401356df0b625ef64d69bf8acc0
Vijay Veerasamy <vijay.veerasamy@gmail.com>
2018-11-26 21:51:51 +0000

    Init

---
+++ src/main/java/com/vijay/restapp/App.java
@@ -0,0 +1,13 @@
+package com.vijay.restapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
